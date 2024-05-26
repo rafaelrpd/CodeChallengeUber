4918540d86a2ec361c0bb746bdac668a919a2c80
Rafael Pereira Dias <p.rafael.dias@viveo.com.br>
2024-05-26 14:48:37 -0300

    Add project files.

---
+++ CodeChallengeUber.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CodeChallengeUber.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
